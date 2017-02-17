3ab769159976b7773efece17aac92e9e6e6ba4b6
Hanna Bulava <anyuta.bulova@mail.ru>
2017-02-17 10:16:08 +0300

    Карта и кнопка

--- js/map.js
+++ js/map.js
@@ -1,8 +1,10 @@
 ymaps.ready(function () {
     var myMap = new ymaps.Map('map', {
             center: [59.9387, 30.327],
-            zoom: 16
-        }, {
+            zoom: 16,
+            controls: []
+        },
+        {
             searchControlProvider: 'yandex#search'
         }),
         myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
