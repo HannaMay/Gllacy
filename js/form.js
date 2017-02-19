79bb7652ee20a85b08db34db0b2c77ab8d5696da
Hanna Bulava <anyuta.bulova@mail.ru>
2017-02-19 10:22:27 +0300

    Анимация формы

---
+++ js/form.js
@@ -0,0 +1,16 @@
+var comlink=document.querySelector(".communication-form__link");
+var popup = document.querySelector(".input");
+var close = popup.querySelector(".button--close");
+var shadow=document.querySelector(".shadow");
+
+comlink.addEventListener("click", function(even){
+  event.preventDefault();
+  shadow.classList.add("shadow-show");
+  popup.classList.add("input-show");
+});
+
+close.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.remove("input-show");
+  shadow.classList.remove("shadow-show");
+});
